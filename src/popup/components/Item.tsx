7380fcc2f5f988fef011d91d1b626fd41aad803f
Manatsawin Hanmongkolchai <git@whs.in.th>
2025-09-03 23:49:23 +0700

    Add icon for open chest and accessibility description

--- src/popup/components/Item.tsx
+++ src/popup/components/Item.tsx
@@ -2,5 +2,5 @@ import { ItemInfo } from "../../data/buddyfarm";
 
 export default (props: {item: ItemInfo}) => {
 	let image = new URL(props.item.image, "https://farmrpg.com");
-	return <span class="inline"><img class="inline h-[1.5em]" src={image.toString()} /> {props.item.name}</span>;
+	return <span class="inline" title={props.item.description}><img class="inline h-[1.5em]" aria-hidden src={image.toString()} alt="" /> {props.item.name}</span>;
 }
--- src/popup/components/Location.tsx
+++ src/popup/components/Location.tsx
@@ -2,5 +2,5 @@ import { LocationInfo } from "../../data/buddyfarm";
 
 export default (props: {location: LocationInfo}) => {
 	let image = new URL(props.location.image, "https://farmrpg.com");
-	return <span class="inline"><img class="inline h-[1.5em]" src={image.toString()} /> {props.location.name}</span>;
+	return <span class="inline"><img class="inline h-[1.5em]" src={image.toString()} aria-hidden alt="" /> {props.location.name}</span>;
 }
